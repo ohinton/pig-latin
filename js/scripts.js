810d63604bcc0c58d42e5335cea7ac8accbbcbc1
Olivia Hinton and Sam Park <olivia@>
2016-06-08 11:53:58 -0700

    add back end logic

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+var consonants = ["B", "C", "D", "F", "G," "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "W", "Z"]
+
+var vowles = ["A", "E", "I", "O", "U", "Y"]
+
+//will we need to convert uses input to uppercase so the code can compare?//
+
+var userInput = function(input) {
+  var yWord = []
+  for (index = 0; index = "y")  {
+    yWord.push(index);
+    yWord.splice(0, 1, 'yay');
+    return yWord;
+}
+  var qWord = []
+  for (index = 0; index = "q") {
+    qWord.push(index);
+    qWord.splice(0, 1, "quay");
+    return qWord;
+  }
+
+  var conWord2 = []
+  for (index = 0; index = consonants; index < 2){
+    conWord2.push(index);
+    // we don't know if we can concat with "&&" and the numbers that refer to the position in the array
+    conWord2.concat(0 && 1 && "ay");
+    // end
+    conWord2.splice(0, 2);
+  }
+
+  var conWord = []
+  for (index = 0; index = consonants; index < 1)  {
+    conWord2.push(index);
+    // we don't know if we can concat with "&&" and the numbers that refer to the position in the array
+    conWord2.concat(0 && "ay");
+    // end
+    conWord.splice(0, 1)
+  }
+
+  var vowelWord = []
+  for (index = 0; index = vowels; index < 1)  {
+    conWord2.push(index);
+    // we don't know if we can concat with "&&" and the numbers that refer to the position in the array
+    conWord2.concat(0 && "ay");
+    // end
+    conWord.splice(0, 1)
+  }
+
+  };
+
+//begin front end logic//
