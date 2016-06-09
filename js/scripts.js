a0d264bb60c76ac4509c5fb5ecfaf41d1b789b09
Olivia Hinton <hinton.olivia@gmail.com>
2016-06-09 15:17:46 -0700

    updated js for vowels and consonants

--- js/scripts.js
+++ js/scripts.js
@@ -8,93 +8,54 @@
     //move that y to end of word
     //and add an ay.
 
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "k", "m", "n", "p", "q", "r", "s", "t", "v", "x","w","z"];
-
 var vowels = ["a", "e", "i", "o", "u"];
 
 var userInput = function(input) {
   var charArray = input.split("");
-  var newcharArray = [];
 
-  //Vowel-words start here
-  for (i=0; i < vowels.length; i++){
-    if (vowels.includes(input.charAt(0))) {
-      newcharArray = charArray.push("ay");
-      //loop through array of vowels here
-      return newcharArray;
-    }
-  }
+  // //Vowel-words start here
+  // for (i=0; i < vowels.length; i++){
+  //   if (vowels[i]===charArray[0]) {
+  //     charArray.push("ay");
+  //     //loop through array of vowels here
+  //     return charArray;
+  //   }
+  // }
   //Vowel-words end here
 
-  //Y-words start here
-    if (input.charAt(0) === "y"){
-      newcharArray = charArray.splice(0, 1);
-      charArray.push("y");
-      charArray.push("ay");
-      return charArray;
+  for (i=0; ;i++){
+    if (vowels.indexOf(charArray[i])>=0){
+      var breakpoint = i;
+      break;
     }
-//Y-words start end
-
-
-
-  // yWord.push(i);
-  // yWord.splice(0, 1, 'yay');
-  // return yWord;
+  }
+  var firstConsonants = charArray.splice(0, breakpoint);
+  charArray = charArray.concat(firstConsonants);
+  charArray.push("ay");
+  return charArray;
 }
+
+var userSentence = function(input) {
+  var finalMessage = "";
+  var wordArray = input.split(" ");
+  wordArray.forEach(function(word) {
+    var translatedWord = userInput(word).join("");
+    finalMessage = finalMessage + translatedWord + " ";
+  });
+  return(finalMessage)
+}
+
+
+
 
 $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
     var inputString = $("#word").val();
-    var result = userInput(inputString);
-    var finalResult = result.join("");
+    var result = userSentence(inputString);
+    var finalResult = result;
 
 
     $("#result").text(finalResult);
   });
 });
-
-
-
-
-//will we need to convert uses input to uppercase so the code can compare?//
-
-
-
-// var qWord = []
-// for (index = 0; index = "q") {
-//   qWord.push(index);
-//   qWord.splice(0, 1, "quay");
-//   return qWord;
-// }
-//
-// var conWord2 = []
-// for (index = 0; index = consonants; index < 2){
-//   conWord2.push(index);
-//   // we don't know if we can concat with "&&" and the numbers that refer to the position in the array
-//   conWord2.concat(0 && 1 && "ay");
-//   // end
-//   conWord2.splice(0, 2);
-// }
-//
-// var conWord = []
-// for (index = 0; index = consonants; index < 1)  {
-//   conWord2.push(index);
-//   // we don't know if we can concat with "&&" and the numbers that refer to the position in the array
-//   conWord2.concat(0 && "ay");
-//   // end
-//   conWord.splice(0, 1)
-// }
-//
-// var vowelWord = []
-// for (index = 0; index = vowels; index < 1)  {
-//   conWord2.push(index);
-//   // we don't know if we can concat with "&&" and the numbers that refer to the position in the array
-//   conWord2.concat(0 && "ay");
-//   // end
-//   conWord.splice(0, 1)
-// }
-//
-
-
-//begin front end logic//
